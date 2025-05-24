a6c8606b37dab306099881eb28866465debbacd1
ravinishayar <remixoldhindi@gmail.com>
2025-05-24 14:12:23 +0530

    Update config.py
    Signed-off-by: ravinishayar <remixoldhindi@gmail.com>

--- config.py
+++ config.py
@@ -30,7 +30,14 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
- # Fill this variable if your upstream repository is private
+UPSTREAM_REPO = getenv(
+    "UPSTREAM_REPO",
+    "https://t.me/MARS_MACHAL_SUPPORT",
+)
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
+GIT_TOKEN = getenv(
+    "GIT_TOKEN", None
+) # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/MARS_MACHAL_SUPPORT")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/MARS_MACHAL_SUPPORT")
